1b791160d3eb28321f305643cf64f5310f98b782
Daniel Costa <danielcosta@MacBook-Pro-de-Daniel.local>
2015-10-22 00:36:22 -0200

    primiero envio com correção dos caminhos

--- application/views/home/login.php
+++ application/views/home/login.php
@@ -19,7 +19,7 @@ License: You must have a valid license purchased only from themeforest(the above
 <head>
     <meta charset="utf-8"/>
 
-    <base href="<?=base_url()?>" />?>
+    <base href="<?=base_url()?>" />
     <title><?=$titulo?></title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
@@ -28,21 +28,21 @@ License: You must have a valid license purchased only from themeforest(the above
     <meta content="" name="author"/>
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
-    <link href="../../assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-    <link href="../../assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
-    <link href="../../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-    <link href="../../assets/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
+    <link href="<?=base_url()?>assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    <link href="<?=base_url()?>assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
+    <link href="<?=base_url()?>assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="<?=base_url()?>assets/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
     <!-- END GLOBAL MANDATORY STYLES -->
     <!-- BEGIN PAGE LEVEL STYLES -->
-    <link href="../../assets/global/plugins/select2/select2.css" rel="stylesheet" type="text/css"/>
-    <link href="../../assets/admin/pages/css/login-soft.css" rel="stylesheet" type="text/css"/>
+    <link href="<?=base_url()?>assets/global/plugins/select2/select2.css" rel="stylesheet" type="text/css"/>
+    <link href="<?=base_url()?>assets/admin/pages/css/login-soft.css" rel="stylesheet" type="text/css"/>
     <!-- END PAGE LEVEL SCRIPTS -->
     <!-- BEGIN THEME STYLES -->
-    <link href="../../assets/global/css/components-rounded.css" id="style_components" rel="stylesheet" type="text/css"/>
-    <link href="../../assets/global/css/plugins.css" rel="stylesheet" type="text/css"/>
-    <link href="../../assets/admin/layout/css/layout.css" rel="stylesheet" type="text/css"/>
-    <link id="style_color" href="../../assets/admin/layout/css/themes/default.css" rel="stylesheet" type="text/css"/>
-    <link href="../../assets/admin/layout/css/custom.css" rel="stylesheet" type="text/css"/>
+    <link href="<?=base_url()?>assets/global/css/components-rounded.css" id="style_components" rel="stylesheet" type="text/css"/>
+    <link href="<?=base_url()?>assets/global/css/plugins.css" rel="stylesheet" type="text/css"/>
+    <link href="<?=base_url()?>assets/admin/layout/css/layout.css" rel="stylesheet" type="text/css"/>
+    <link id="style_color" href="<?=base_url()?>assets/admin/layout/css/themes/default.css" rel="stylesheet" type="text/css"/>
+    <link href="<?=base_url()?>assets/admin/layout/css/custom.css" rel="stylesheet" type="text/css"/>
     <!-- END THEME STYLES -->
     <link rel="shortcut icon" href="favicon.ico"/>
 </head>
@@ -52,7 +52,7 @@ License: You must have a valid license purchased only from themeforest(the above
 <!-- BEGIN LOGO -->
 <div class="logo">
     <a href="index.html">
-        <img src="../../assets/admin/layout3/img/logo-big.png" alt=""/>
+        <img src="<?=base_url()?>assets/admin/layout3/img/logo-big.png" alt=""/>
     </a>
 </div>
 <!-- END LOGO -->
