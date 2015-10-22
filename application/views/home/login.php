0c1a8b8549d0d59da3432a54517cb4b2161fe7a5
Daniel Costa <danielcosta@MacBook-Pro-de-Daniel.local>
2015-10-22 00:51:59 -0200

    primiero envio com correção dos caminhos

--- application/views/home/login.php
+++ application/views/home/login.php
@@ -134,26 +134,26 @@ License: You must have a valid license purchased only from themeforest(the above
 <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
 <!-- BEGIN CORE PLUGINS -->
 <!--[if lt IE 9]>
-<script src="../../assets/global/plugins/respond.min.js"></script>
-<script src="../../assets/global/plugins/excanvas.min.js"></script>
+<script src="<?=base_url()?>assets/global/plugins/respond.min.js"></script>
+<script src="<?=base_url()?>assets/global/plugins/excanvas.min.js"></script>
 <![endif]-->
-<script src="../../assets/global/plugins/jquery.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jquery-migrate.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/jquery.cokie.min.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/global/plugins/jquery-migrate.min.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/global/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/global/plugins/jquery.cokie.min.js" type="text/javascript"></script>
 <!-- END CORE PLUGINS -->
 <!-- BEGIN PAGE LEVEL PLUGINS -->
-<script src="../../assets/global/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
-<script src="../../assets/global/plugins/backstretch/jquery.backstretch.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="../../assets/global/plugins/select2/select2.min.js"></script>
+<script src="<?=base_url()?>assets/global/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/global/plugins/backstretch/jquery.backstretch.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="<?=base_url()?>assets/global/plugins/select2/select2.min.js"></script>
 <!-- END PAGE LEVEL PLUGINS -->
 <!-- BEGIN PAGE LEVEL SCRIPTS -->
-<script src="../../assets/global/scripts/metronic.js" type="text/javascript"></script>
-<script src="../../assets/admin/layout/scripts/layout.js" type="text/javascript"></script>
-<script src="../../assets/admin/layout/scripts/demo.js" type="text/javascript"></script>
-<script src="../../assets/admin/pages/scripts/login-soft.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/global/scripts/metronic.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/admin/layout/scripts/layout.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/admin/layout/scripts/demo.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/admin/pages/scripts/login-soft.js" type="text/javascript"></script>
 <!-- END PAGE LEVEL SCRIPTS -->
 <script>
     jQuery(document).ready(function() {
@@ -163,10 +163,10 @@ License: You must have a valid license purchased only from themeforest(the above
         Demo.init();
         // init background slide images
         $.backstretch([
-                "../../assets/admin/pages/media/bg/1.jpg",
-                "../../assets/admin/pages/media/bg/5.jpg",
-                "../../assets/admin/pages/media/bg/6.jpg",
-                "../../assets/admin/pages/media/bg/7.jpg"
+                "<?=base_url()?>assets/admin/pages/media/bg/1.jpg",
+                "<?=base_url()?>assets/admin/pages/media/bg/5.jpg",
+                "<?=base_url()?>assets/admin/pages/media/bg/6.jpg",
+                "<?=base_url()?>assets/admin/pages/media/bg/7.jpg"
             ], {
                 fade: 1000,
                 duration: 8000
